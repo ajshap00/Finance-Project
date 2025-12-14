0a2f172c0d7ea79d0a15945025c7652fecf8dd28
Alex Shapiro <ajshap00@gmail.com>
2025-12-14 11:47:12 -0500

    plot changes

--- plots.py
+++ plots.py
@@ -11,21 +11,6 @@ def show_plot():
 
     df["Balance"] = df["Amount"].cumsum()
 
-    monthly = (
-        df
-        .groupby(df["Date"].dt.to_period("M"))["Amount"]
-        .sum()
-    )
-    monthly.index = monthly.index.to_timestamp()
-
-    yearly = (
-        df
-        .groupby(df["Date"].dt.to_period("Y"))["Amount"]
-        .sum()
-    )
-
-    yearly.index = yearly.index.to_timestamp()
-
     summary = df.assign(
         Earnings=df["Amount"].where(df["Amount"] > 0, 0),
         Spending=df["Amount"].where(df["Amount"] < 0, 0)
@@ -33,20 +18,20 @@ def show_plot():
 
     monthly_summary = summary.groupby(
         summary["Date"].dt.to_period("M")
-    )[["Earnings", "Spending"]].sum()
+    )[["Earnings", "Spending"]].sum().abs()
 
-    monthly_summary.index = monthly_summary.index.strftime("%b %Y")
-
-    # make spending positive for bar chart
-    monthly_summary["Spending"] = monthly_summary["Spending"].abs()
-
+    monthly_summary.index = monthly_summary.index.strftime("%b %y")
+    
     ax = monthly_summary.plot(
         kind="bar",
         figsize=(10,4),
         width=0.8
     )
+    ax.bar_label(ax.containers[0])
+    ax.bar_label(ax.containers[1])
     ax.set_ylim(0, monthly_summary.max().max() * 1.1)
     ax.margins(y=0.05)
+    mp.xticks(rotation=0)
 
     mp.title("Monthly Earnings vs Spending")
     mp.xlabel("Month")
